2b965abf885aa9203c854107a14e41e2c352ccf8
Ferdy Luqman Djunaedi <ferdyluqman17@gmail.com>
2023-08-10 01:23:43 +0700

    update

--- app.py
+++ app.py
@@ -25,38 +25,47 @@ def webhook():
         sub_msg = input("Anda memilih Wisata. Silakan pilih jenis wisata yang ingin kamu ketahui:\n1. Wisata Alam\n2. Wisata Religi\n3. Wisata Buatan")
         
         if sub_msg.lower() == "wisata alam":
-            alam_msg = input("Anda memilih Wisata Alam. Silakan pilih opsi:\n1. Tempat Wisata Alam A\n2. Tempat Wisata Alam B\n3. Tempat Wisata Alam C")
-            if alam_msg == "1":
-                response = "Anda memilih Tempat Wisata Alam A. Berikut informasinya..."
-            elif alam_msg == "2":
-                response = "Anda memilih Tempat Wisata Alam B. Berikut informasinya..."
-            elif alam_msg == "3":
-                response = "Anda memilih Tempat Wisata Alam C. Berikut informasinya..."
-            else:
-                response = "Maaf, pilihan Tempat Wisata Alam tidak valid."
-        
+            while True:
+                alam_msg = input("Anda memilih Wisata Alam. Silakan pilih opsi:\n1. Tempat Wisata Alam A\n2. Tempat Wisata Alam B\n3. Tempat Wisata Alam C\n0. Kembali ke Menu Wisata")
+                if alam_msg == "1":
+                    response = "Anda memilih Tempat Wisata Alam A. Berikut informasinya..."
+                elif alam_msg == "2":
+                    response = "Anda memilih Tempat Wisata Alam B. Berikut informasinya..."
+                elif alam_msg == "3":
+                    response = "Anda memilih Tempat Wisata Alam C. Berikut informasinya..."
+                elif alam_msg == "0":
+                    break  # Kembali ke menu utama
+                else:
+                    response = "Maaf, pilihan Tempat Wisata Alam tidak valid."
+
         elif sub_msg.lower() == "wisata religi":
-            religi_msg = input("Anda memilih Wisata Religi. Silakan pilih opsi:\n1. Tempat Wisata Religi A\n2. Tempat Wisata Religi B\n3. Tempat Wisata Religi C")
-            if religi_msg == "1":
-                response = "Anda memilih Tempat Wisata Religi A. Berikut informasinya..."
-            elif religi_msg == "2":
-                response = "Anda memilih Tempat Wisata Religi B. Berikut informasinya..."
-            elif religi_msg == "3":
-                response = "Anda memilih Tempat Wisata Religi C. Berikut informasinya..."
-            else:
-                response = "Maaf, pilihan Tempat Wisata Religi tidak valid."
-        
+            while True:
+                religi_msg = input("Anda memilih Wisata Religi. Silakan pilih opsi:\n1. Tempat Wisata Religi A\n2. Tempat Wisata Religi B\n3. Tempat Wisata Religi C\n0. Kembali ke Menu Wisata")
+                if religi_msg == "1":
+                    response = "Anda memilih Tempat Wisata Religi A. Berikut informasinya..."
+                elif religi_msg == "2":
+                    response = "Anda memilih Tempat Wisata Religi B. Berikut informasinya..."
+                elif religi_msg == "3":
+                    response = "Anda memilih Tempat Wisata Religi C. Berikut informasinya..."
+                elif religi_msg == "0":
+                    break  # Kembali ke menu utama
+                else:
+                    response = "Maaf, pilihan Tempat Wisata Religi tidak valid."
+
         elif sub_msg.lower() == "wisata buatan":
-            buatan_msg = input("Anda memilih Wisata Buatan. Silakan pilih opsi:\n1. Tempat Wisata Buatan A\n2. Tempat Wisata Buatan B\n3. Tempat Wisata Buatan C")
-            if buatan_msg == "1":
-                response = "Anda memilih Tempat Wisata Buatan A. Berikut informasinya..."
-            elif buatan_msg == "2":
-                response = "Anda memilih Tempat Wisata Buatan B. Berikut informasinya..."
-            elif buatan_msg == "3":
-                response = "Anda memilih Tempat Wisata Buatan C. Berikut informasinya..."
-            else:
-                response = "Maaf, pilihan Tempat Wisata Buatan tidak valid."
-        
+            while True:
+                buatan_msg = input("Anda memilih Wisata Buatan. Silakan pilih opsi:\n1. Tempat Wisata Buatan A\n2. Tempat Wisata Buatan B\n3. Tempat Wisata Buatan C\n0. Kembali ke Menu Wisata")
+                if buatan_msg == "1":
+                    response = "Anda memilih Tempat Wisata Buatan A. Berikut informasinya..."
+                elif buatan_msg == "2":
+                    response = "Anda memilih Tempat Wisata Buatan B. Berikut informasinya..."
+                elif buatan_msg == "3":
+                    response = "Anda memilih Tempat Wisata Buatan C. Berikut informasinya..."
+                elif buatan_msg == "0":
+                    break  # Kembali ke menu utama
+                else:
+                    response = "Maaf, pilihan Tempat Wisata Buatan tidak valid."
+
         else:
             response = "Maaf, pilihan jenis wisata tidak valid."
     # ... (sisa kode untuk hotel dan oleh-oleh khas)
